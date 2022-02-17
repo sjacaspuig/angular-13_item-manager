ec8704269e8e5003082f6953371ec3ac4fd09376
sjacas <sergi.jacas@wezaam.com>
2022-02-17 05:19:50 +0100

    Implemented as single page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Item } from './models/item.interface';
+import { TableConfiguration } from './models/table-configuration.interface';
+import { ItemsService } from './services/items/items.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'single-page-app';
+export class AppComponent implements OnInit {
+  
+  items$: Observable<Item[]> | undefined;
+  configuration: TableConfiguration[] = [
+    {id: 'title', title: 'Title', type: 'text'},
+    {id: 'description', title: 'Description', type: 'text'},
+    {id: 'price', title: 'Price', type: 'number', textToAdd: '€'},
+    {id: 'email', title: 'Contact',type: 'text'},
+    {id: 'image', title: 'Photo', type: 'img'},
+  ]
+
+  constructor(
+    private itemsService: ItemsService
+  ) {}
+
+  ngOnInit(): void {
+    this.items$ = this.itemsService.getItems();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { TableModule } from './components/table/table.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    TableModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
