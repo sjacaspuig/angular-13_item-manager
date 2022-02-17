871bedf5a35dd8fe59266304c42269367b99f224
sjacas <sergi.jacas@wezaam.com>
2022-02-17 05:18:44 +0100

    Created a component called searcher to don't repeat code

---
+++ src/app/components/searcher/searcher.component.ts
@@ -0,0 +1,37 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { debounceTime, fromEvent, map } from 'rxjs';
+
+@Component({
+  selector: 'searcher',
+  templateUrl: './searcher.component.html',
+  styleUrls: ['./searcher.component.scss']
+})
+export class SearcherComponent implements OnInit {
+
+  //Inputs
+  @Input() placeholder: string = 'Search';
+  @Input() id: string = 'Search';
+  
+  // Outputs
+  @Output() onSearch: EventEmitter<EventTarget> = new EventEmitter<EventTarget>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    const searcher = document.getElementsByTagName('input');
+
+    if(searcher) {
+
+      fromEvent(searcher, 'input').pipe(
+        debounceTime(300),
+        map((event: Event) => event.target )
+      ).subscribe((target: EventTarget | null) => {
+        if(target) {
+          this.onSearch.emit(target);
+        }
+      })
+    }
+  }
+
+}
---
+++ src/app/components/searcher/searcher.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SearcherComponent } from './searcher.component';
+
+
+
+@NgModule({
+  declarations: [
+    SearcherComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    SearcherComponent
+  ]
+})
+export class SearcherModule { }
